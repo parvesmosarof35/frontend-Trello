af84b2a4d4d82c929beabc01641255e4f08f975e
parvesmosarof35 <parves@example.com>
2026-09-03 15:07:17 +0600

    fix(docker): enable standalone output in next.config.ts and add public directory

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
